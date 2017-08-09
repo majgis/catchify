b7a9dddeeb6612e965d13c8668532fffa4a567fb
Michael Jackson <majgis@gmail.com>
2017-08-08 21:11:01 -0600

    Make then and catch functions static

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
+function onThen (v){
+  return [null, v];
+}
+
+function onCatch (err){
+  return [err]
+}
+
 function catchify (p){
   return p
-    .then(v=>[null,v])
-    .catch(e=>[e]);
+    .then(onThen)
+    .catch(onCatch);
 }
 
 module.exports = catchify;
